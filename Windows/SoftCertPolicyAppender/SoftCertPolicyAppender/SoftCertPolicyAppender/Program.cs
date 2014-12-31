e24764a0c0ef91f77a4642d9aee3dbea204cb6ea
lhyqy5 <lhyqy5@gmail.com>
2014-12-31 16:45:54 +0800

    add a tool that can add soft certificate policy(just disallow policy) in commandline

---
+++ Windows/SoftCertPolicyAppender/SoftCertPolicyAppender/SoftCertPolicyAppender/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace SoftCertPolicyAppender
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+            var cers = args.Where(x => x.EndsWith(".cer") || x.EndsWith(".crt") || x.EndsWith(".pem"));
+
+            foreach (var s in cers)
+            {
+                try
+                {
+                    var appdender = new CertPolicyAppender();
+                    appdender.Load(s);
+                    appdender.WriteRegisty();
+                    appdender.AddCertPolicy();
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(e);
+                    Console.ResetColor();
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+        }
+    }
+}
