5a34e99df7151850fec82774ada8d59c6d898428
lhyqy5 <lhyqy5@gmail.com>
2015-01-21 09:15:41 +0800

    format code

--- Windows/SoftCertPolicyAppender/SoftCertPolicyAppender/SoftCertPolicyAppender/Program.cs
+++ Windows/SoftCertPolicyAppender/SoftCertPolicyAppender/SoftCertPolicyAppender/Program.cs
@@ -9,55 +9,55 @@ namespace SoftCertPolicyAppender
         static void Main(string[] args)
         {
             var flag = 0;
-			var cers = args.Where(x => x.EndsWith(".cer") || x.EndsWith(".crt") || x.EndsWith(".pem")).ToArray();
+            var cers = args.Where(x => x.EndsWith(".cer") || x.EndsWith(".crt") || x.EndsWith(".pem")).ToArray();
             if (args.Contains("-r"))
             {
                 flag = 1;
             }
 
-			if (args.Contains("--set-force"))
-			{
-				var appender = new CertPolicyAppender ();
-				appender.SetForceRegistryPolicyStat (true);
-				appender.SetForcePolicyStat (true);
-				Console.WriteLine("Apply force certificate policy");
-			}
+            if (args.Contains("--set-force"))
+            {
+                var appender = new CertPolicyAppender ();
+                appender.SetForceRegistryPolicyStat (true);
+                appender.SetForcePolicyStat (true);
+                Console.WriteLine("Apply force certificate policy");
+            }
 
-			if (args.Contains("--unset-force"))
-			{
-				var appender = new CertPolicyAppender ();
-				appender.SetForceRegistryPolicyStat (false);
-				appender.SetForcePolicyStat (false);
-				Console.WriteLine("Cancel force certificate policy");
-			}
+            if (args.Contains("--unset-force"))
+            {
+                var appender = new CertPolicyAppender ();
+                appender.SetForceRegistryPolicyStat (false);
+                appender.SetForcePolicyStat (false);
+                Console.WriteLine("Cancel force certificate policy");
+            }
 
-			for (var i=0 ;i<cers.Length;i++)
+            for (var i=0 ;i<cers.Length;i++)
             {
                 try
                 {
                     var appdender = new CertPolicyAppender();
-					appdender.Load(cers[i]);
-					Console.ForegroundColor = ConsoleColor.DarkGreen;
-					Console.Write("{0}.",i+1);
-					Console.ResetColor();
+                    appdender.Load(cers[i]);
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.Write("{0}.",i+1);
+                    Console.ResetColor();
                     switch (flag)
                     {
                         case 0:
                             appdender.WriteRegisty();
                             appdender.AddCertPolicy();
-						Console.Write("Add cert policy for ");
+                        Console.Write("Add cert policy for ");
 
                             break;
                         case 1:
                             appdender.RemoveRegisty();
                             appdender.RemoveCertPolicy();
-						Console.Write("Remove cert policy for ");
+                        Console.Write("Remove cert policy for ");
                             break;                            
                     }
-					Console.ForegroundColor = ConsoleColor.Yellow;
-					Console.WriteLine("{0}({1})",appdender.Certificate.Subject,appdender.Certificate.Thumbprint);
-					Console.ResetColor();
-				}
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("{0}({1})",appdender.Certificate.Subject,appdender.Certificate.Thumbprint);
+                    Console.ResetColor();
+                }
                 catch (Exception e)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
