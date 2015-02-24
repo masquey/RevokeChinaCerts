6b7fbe764a81de26bfd28c031d5457f7c4615162
phoeagon <phoeagon@gmail.com>
2015-02-24 21:18:03 +0800

    add flashable.zips, updated Cert path according to d4e340c429edb

--- Android/generate.sh
+++ Android/generate.sh
@@ -5,21 +5,21 @@ set -e
 if [ ${1:-extended} = 'all' ];then
     echo "Generating ALL CRL set"
     # TODO: Explicitly distinguish between CA & EE certificates.
-    CA_CERTS=`ls ../Windows/Certs/*.crt`
-    EE_CERTS=`ls ../Windows/Certs/\[Fake\]*.crt`
+    CA_CERTS=`ls ../Windows/Certs/Online/*.crt`
+    EE_CERTS=`ls ../Windows/Certs/Online/\[Fake\]*.crt`
     echo "all"
 elif [ ${1:-extended} = 'extended' ];then
     echo "Generating EXTENDED CRL set"
-    CA_CERTS=`ls ../Windows/Certs/CNNIC_*.crt ../Windows/Certs/China_Internet_Network_Information_Center_EV_Certificates_Root.crt ../Windows/Certs/[Suspicious]WaccBaiduCom.crt ../Windows/Certs/GiantRootCA.crt ../Windows/Certs/CFCA_*.crt  ../Windows/Certs/UCA_*.crt  ../Windows/Certs/[Suspicious]GoAgent_CA.crt`
-    EE_CERTS=`ls ../Windows/Certs/\[Fake\]*.crt`
+    CA_CERTS=`ls ../Windows/Certs/Online/CNNIC_*.crt ../Windows/Certs/Online/China_Internet_Network_Information_Center_EV_Certificates_Root.crt ../Windows/Certs/Online/[Suspicious]WaccBaiduCom.crt ../Windows/Certs/Online/GiantRootCA.crt ../Windows/Certs/Online/CFCA_*.crt  ../Windows/Certs/Online/UCA_*.crt  ../Windows/Certs/Online/[Suspicious]GoAgent_CA.crt`
+    EE_CERTS=`ls ../Windows/Certs/Online/\[Fake\]*.crt`
 elif [ ${1:-extended} = 'restore' ];then
     echo "Generating RESTORE CRL set"
     CA_CERTS=''
     EE_CERTS=''
 else
     echo "Generating Basic CRL set"
-    CA_CERTS=`ls ../Windows/Certs/CNNIC_*.crt ../Windows/Certs/China_Internet_Network_Information_Center_EV_Certificates_Root.crt ../Windows/Certs/[Suspicious]WaccBaiduCom.crt ../Windows/Certs/GiantRootCA.crt`
-    EE_CERTS=`ls ../Windows/Certs/\[Fake\]*.crt`
+    CA_CERTS=`ls ../Windows/Certs/Online/CNNIC_*.crt ../Windows/Certs/Online/China_Internet_Network_Information_Center_EV_Certificates_Root.crt ../Windows/Certs/Online/[Suspicious]WaccBaiduCom.crt ../Windows/Certs/Online/GiantRootCA.crt`
+    EE_CERTS=`ls ../Windows/Certs/Online/\[Fake\]*.crt`
 fi
 
 echo "Generating Configurations"
