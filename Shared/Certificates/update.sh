ffe1daa4e33eaed8e4d36e198a22ef57ebb1b42d
phoeagon <phoeagon@gmail.com>
2015-04-04 18:43:36 +0800

    update Android hook

--- Shared/Certificates/update.sh
+++ Shared/Certificates/update.sh
@@ -1,4 +1,6 @@
-#!/bin/bash 
-ls hooks | while read name; do
-	hooks/$name
+#!/bin/bash
+DIR=`dirname $0`
+ls $DIR/hooks | while read name; do
+	$DIR/hooks/$name
+	echo "$DIR/hooks/$name" executed!
 done
