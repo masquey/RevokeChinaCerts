0e26813ecd56f03f1ae13b07ded019e78c75d645
phoeagon <phoeagon@gmail.com>
2015-04-05 23:42:30 +0800

    remove non-sorted certsum files from ./Android

--- Shared/Certificates/hooks/linux.sh
+++ Shared/Certificates/hooks/linux.sh
@@ -6,15 +6,14 @@ CERTIFICATES="$SOURCE_DIR/../certificates.sh"
 
 
 function _generate {
-	rm "$2" "$3"
-	SEVERITY="$1" TYPE="Root.CA Intermediate.CA SSL" "$CERTIFICATES" \
+	rm "$2"
+	(SEVERITY="$1" TYPE="Root.CA Intermediate.CA SSL" "$CERTIFICATES" \
 	| while read cert; do
-		paste <(openssl x509 -sha1 -fingerprint -noout -in $cert) <(git ls-tree --full-name --name-only HEAD $cert) >> $2
-	done
-	sort "$2" > "$3"
+		paste <(openssl x509 -sha1 -fingerprint -noout -in $cert) <(git ls-tree --full-name --name-only HEAD $cert)
+	done) | sort > "$2"
 }
 
 DEST_DIR="$SOURCE_DIR/../../../Linux"
-_generate "High" "$DEST_DIR/certsum_base.txt" "$DEST_DIR/certsum_base_sorted.txt"
-_generate "High Medium" "$DEST_DIR/certsum_extended.txt" "$DEST_DIR/certsum_extended_sorted.txt"
-_generate "High Medium Low" "$DEST_DIR/certsum_all.txt" "$DEST_DIR/certsum_all_sorted.txt"
+_generate "High" "$DEST_DIR/certsum_base_sorted.txt"
+_generate "High Medium" "$DEST_DIR/certsum_extended_sorted.txt"
+_generate "High Medium Low" "$DEST_DIR/certsum_all_sorted.txt"
